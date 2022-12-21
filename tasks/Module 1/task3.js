6171b0d76353f3461a63c451b89146dd702c21a5
anastasia-sl <anastasiaslynko2206@gmail.com>
2022-12-21 20:39:56 +0200

    task3

---
+++ tasks/Module 1/task3.js
@@ -0,0 +1,11 @@
+let sum = (a, b) => (a + b);
+let summa=sum(2,50);
+let i = function (c){
+if (c<100){
+return console.log('Число меньше 100');
+}else if(c>=100){
+return console.log("...Мы не знаем что это такое");
+}
+};
+let res = i(summa);
+console.log(res);
