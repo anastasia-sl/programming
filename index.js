f970203e1566fb504da77abe6ca819c2c9fff539
anastasia-sl <anastasiaslynko2206@gmail.com>
2022-12-21 19:19:02 +0200

    task1

---
+++ index.js
@@ -0,0 +1,21 @@
+let object = {
+    a: 10,
+    b: 30
+};
+let ar = [object, object, object, 12, 14, 15];
+
+console.log(JSON.stringify(ar));
+console.log(ar);
+
+ar[0].b=20;
+
+if(a===20 && b===30){
+
+}else if(a===20 || b===30) {
+
+}else{
+
+}
+
+
+
