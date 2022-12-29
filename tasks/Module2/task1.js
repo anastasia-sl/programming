c4d11e810af46b29536b2d5eb4cd38bacd52cb9c
pylypenko-vitalii-easternpeak <80914633+pylypenko-vitalii-easternpeak@users.noreply.github.com>
2022-12-29 18:12:17 +0200

    L6 домашка на переворачивание массива

--- tasks/Module2/task1.js
+++ tasks/Module2/task1.js
@@ -2,19 +2,22 @@ function fnctn(obj){
     if (typeof obj !== 'object') {
         return obj;
     }
+
     let result;
 
-    if(Array.isArray(obj)){
+    if (!Array.isArray(obj)){
       result=[...obj];
       let counter=0;
+
       for (let field of result) {
             if (typeof field === 'object') {
                 result[counter] = fnctn(field);
             }
             counter++;
         }
-    }else{
+    } else {
         result={...obj};
+
         for (let field in result) {
             if(typeof result[field] === 'object'){
                 result[field]=fnctn(result[field]);
@@ -22,14 +25,9 @@ function fnctn(obj){
         }
     }
 
-
-
-
-
-
-
     return result;
 }
+
 let cat={
     color:'grey',
     name:'kate',
