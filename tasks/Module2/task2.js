1ba5011f0d824dc8299dfdd73e687b1c80120ba6
anastasia-sl <anastasiaslynko2206@gmail.com>
2023-01-04 15:38:46 +0200

    task4

---
+++ tasks/Module2/task2.js
@@ -0,0 +1,15 @@
+
+function a(obj){
+    if(!Array.isArray(obj)){
+       throw new Error('Argument is not an Array');
+    }
+    let newArr;
+    for(let i = obj.length-1; i >= 0; i--){
+        newArr = obj.push(i);
+    }
+    return newArr;
+}
+
+
+const arr=[1, 10, 4, 8,];
+console.log(a(arr));
